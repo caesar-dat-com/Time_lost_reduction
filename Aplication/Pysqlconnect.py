93c1cb2298f4e521b155886a77e752c44dd8b4b9
JoanMz <joanmzteo@gmail.com>
2024-03-06 20:37:26 -0500

    arch

--- Aplication/Pysqlconnect.py
+++ Aplication/Pysqlconnect.py
@@ -1,22 +1,42 @@
 """This part controls the connection with the database on mysql with mysql connector"""
-import mysql.connector
-from decouple import config as dbconfig
+from decouple import config
+from datime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-def baseconnect():
-    try:
-        mydb = mysql.connector.connect(
-            host = dbconfig("MYSQL_HOST"),
-            user = dbconfig("MYSQL_USER"),
-            password = dbconfig("MYSQL_PASSWORD"),
-            database = dbconfig("MYSQL_DB")
-            )
-        cur = mydb.cursor()
-        print("Succesfull Connection")
-        return cur ,mydb 
-    except mysql.connector.Error as err:
-        print(f"error {err}")
-        return None, None
+engine = create_engine(f"postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@{config("DB_HOST")}/{config("DB_DATABASE")}")
+base = declarative_base()
+
+class act_programados(base):
+    __tablename__ = config("DB_TABLE1")
+    def __str__(self):
+        self.username
+class act_no_programados(base):
+    __tablename__ = config("DB_TABLE2")
+    def __str__(self):
+        self.username
+class ins_no_programados(base):
+    __tablename__ = config("DB_TABLE3")
+    def __str__(self):
+        self.username
+
+class ins_programados(base):
+    __tablename__ = config("DB_TABLE4")
+    def __str(self):
+        self.username
+    
+class ins_programados(base):
+    __tablename__ = config("DB_TABLE5")
+    
+    def __str__(self):
+        self.username
 
 
+Session = sessionmaker(engine)
+session = Session()
 
+def connection(engine=engine):
+    return engine
 
+    
